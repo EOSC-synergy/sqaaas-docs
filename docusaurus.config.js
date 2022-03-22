58aa306ef7d917544889d86d9b9208363fb97934
orviz <orviz@ifca.unican.es>
2022-03-22 12:07:49 +0100

    Move baseUrl from  /sqaaas-docs/ to /

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'SQAaaS documentation site',
   tagline: 'Building quality research software',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/sqaaas-docs/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
