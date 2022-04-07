b829bf61350115ee7fdcc7f28ca6d47f4171992c
Pablo Orviz <orviz@ifca.unican.es>
2022-04-07 10:24:21 +0200

    Add steps in the menu under a new subcategory

--- sidebars.js
+++ sidebars.js
@@ -30,7 +30,15 @@ module.exports = {
       label: 'Pipeline as a Service',
       items: [
           'pipeline_as_a_service/intro',
-          'pipeline_as_a_service/the_3_step_process'
+          'pipeline_as_a_service/the_3_step_process',
+          {
+              type: 'category',
+              label: 'Pipeline composition steps',
+              items: [
+                  'pipeline_as_a_service/step_1_repositories',
+                  'pipeline_as_a_service/step_2_tooling'
+              ]
+          }
       ]
     },
     {
